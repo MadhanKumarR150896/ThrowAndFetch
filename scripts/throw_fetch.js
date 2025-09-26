08f715dc2feb49a085b9a32ba13ea7d3cab3f5e7
MadhanKumarR150896 <madhankumar150896@outlook.com>
2025-09-26 20:05:29 +0530

    Adding JS , adjusting CSS hierarchy and tuned the delay time

--- scripts/throw_fetch.js
+++ scripts/throw_fetch.js
@@ -1,14 +1,34 @@
+const background = document.querySelector('.js-background');
+
+function throwBall() {
+  background.classList.remove('play');
+  void background.offsetWidth;
+  background.classList.add('play');
+};
+
+function stopPLay() {
+  background.classList.remove('play');
+};
 
 
+document.querySelector('.js-ball')
+  .addEventListener('click', () => {
+    throwBall();
+  });
 
-  document.querySelector('.js-ball')
-    .addEventListener('click', () => {
-      throwBall();
-    })
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'b') {
+    throwBall();
+  }
+});
 
+document.querySelector('.js-sun')
+  .addEventListener('click', () => {
+    stopPLay();
+  });
 
-    isPlaying = false;
-    
-    function throwBall () {
-      
-    }
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 's') {
+    stopPLay();
+  }
+});
